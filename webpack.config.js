c8db905b9de6f021b273069eff12c9bf94adcc42
Dana Zhang <danadaners@gmail.com>
2020-11-30 20:21:17 -0500

    finally got service worker working????

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,35 @@
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 module.exports = {
   mode: "development",
   entry: [
     "@babel/polyfill", // enables async-await
     "./client/index.js"
+  ],
+  plugins: [
+    new WorkboxPlugin.GenerateSW({
+      // Do not precache images
+      exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+
+      // Define runtime caching rules.
+      runtimeCaching: [{
+        // Match any request that ends with .png, .jpg, .jpeg or .svg.
+        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+        // Apply a cache-first strategy.
+        handler: 'CacheFirst',
+
+        options: {
+          // Use a custom cache name.
+          cacheName: 'images',
+
+          // Only cache 10 images.
+          expiration: {
+            maxEntries: 10,
+          },
+        },
+      }],
+    })
   ],
   output: {
     path: __dirname,
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "public/",
+  "globPatterns": [
+    "**/*.{js,ico,html,png,json,txt,css}"
+  ],
+  "swDest": "public/service-worker.js"
+};
